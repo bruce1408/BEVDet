6c416f37c3eafcac2573e3539526863eec2307d3
bruce1408 <summer56567@163.com>
2022-10-27 16:30:08 +0800

    inception code adjust lr policy

--- tools/analysis_tools/get_flops.py
+++ tools/analysis_tools/get_flops.py
@@ -31,21 +31,22 @@ def parse_args():
         nargs='+',
         action=DictAction,
         help='override some settings in the used config, the key-value pair '
-        'in xxx=yyy format will be merged into config file. If the value to '
-        'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
-        'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
-        'Note that the quotation marks are necessary and that no white space '
-        'is allowed.')
+             'in xxx=yyy format will be merged into config file. If the value to '
+             'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
+             'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
+             'Note that the quotation marks are necessary and that no white space '
+             'is allowed.')
     args = parser.parse_args()
     return args
 
-def construct_input(input_shape):
-    rot = torch.eye(3).float().cuda().view(1,3,3)
-    rot = torch.cat([rot for _ in range(6)],axis=0).view(1,6,3,3)
 
-    input =dict(img_inputs=[
+def construct_input(input_shape):
+    rot = torch.eye(3).float().cuda().view(1, 3, 3)
+    rot = torch.cat([rot for _ in range(6)], axis=0).view(1, 6, 3, 3)
+
+    input = dict(img_inputs=[
         torch.ones(()).new_empty(
-                (1, 6, 3, *input_shape)).cuda(),
+            (1, 6, 3, *input_shape)).cuda(),
         rot,
         torch.ones((1, 6, 3)).cuda(),
         rot,
@@ -53,8 +54,8 @@ def construct_input(input_shape):
         torch.ones((1, 6, 3)).cuda()])
     return input
 
-def main():
 
+def main():
     args = parse_args()
 
     if args.modality == 'point':
@@ -64,7 +65,7 @@ def main():
         if len(args.shape) == 1:
             input_shape = (3, args.shape[0], args.shape[0])
         elif len(args.shape) == 2:
-            input_shape = (3, ) + tuple(args.shape)
+            input_shape = (3,) + tuple(args.shape)
         else:
             raise ValueError('invalid input shape')
     elif args.modality == 'multi':
