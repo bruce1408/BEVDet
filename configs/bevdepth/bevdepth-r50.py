9044e7754caceb1f91cb56bd1ca15c4c2f6842a3
bruce1408 <summer56567@163.com>
2022-11-01 23:50:20 +0800

    modify bevdepth save checkpoints path

--- configs/bevdepth/bevdepth-r50.py
+++ configs/bevdepth/bevdepth-r50.py
@@ -255,6 +255,6 @@ lr_config = dict(
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=0.001,
-    gamma=0.9,
-    step=[16, 22, 25, 27, 28, 29, 30, 32, 33, 35, 37, 39, 42, 50, 55])
+    gamma=0.8,
+    step=[13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 25, 27, 28, 29, 30, 32, 33, 35, 37, 39, 42, 50, 55])
 runner = dict(type='EpochBasedRunner', max_epochs=100)
